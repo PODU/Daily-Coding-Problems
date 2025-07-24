0280ba04082cf22329729bd091d46b14a2db2316
Poduval Aditya Anil <podu2997@gmail.com>
2025-07-24 11:10:11 +0530

    Day #25 (Java)

---
+++ Day #25/Solution.java
@@ -0,0 +1,29 @@
+// Regex matching with '.' and '*' via DP. Time O(m*n), Space O(m*n).
+public class Solution {
+    static boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+        for (int j = 1; j <= n; j++)
+            if (p.charAt(j - 1) == '*') dp[0][j] = dp[0][j - 2];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    if (p.charAt(j - 2) == '.' || p.charAt(j - 2) == s.charAt(i - 1))
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                } else if (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        assert isMatch("aa", "a*");
+        assert isMatch("ab", ".*");
+        assert !isMatch("mississippi", "mis*is*p*.");
+        System.out.println(isMatch("ray", "ra.") ? "true" : "false");
+    }
+}
