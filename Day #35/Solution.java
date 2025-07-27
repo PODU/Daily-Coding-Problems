ab130ee226f51400fa33df5dd042f124452d7f9f
Poduval Aditya Anil <podu2997@gmail.com>
2025-07-27 12:36:11 +0530

    Day #35: Java solution

---
+++ Day #35/Solution.java
@@ -0,0 +1,23 @@
+// Dutch National Flag 3-way partition (R<G<B). In-place, O(n) time, O(1) space, swaps only.
+public class Solution {
+    static void sortRGB(char[] a) {
+        int low = 0, mid = 0, high = a.length - 1;
+        while (mid <= high) {
+            if (a[mid] == 'R') { char t = a[low]; a[low] = a[mid]; a[mid] = t; low++; mid++; }
+            else if (a[mid] == 'G') { mid++; }
+            else { char t = a[mid]; a[mid] = a[high]; a[high] = t; high--; }
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] a = {'G', 'B', 'R', 'R', 'B', 'R', 'G'};
+        sortRGB(a);
+        StringBuilder sb = new StringBuilder("[");
+        for (int i = 0; i < a.length; i++) {
+            sb.append("'").append(a[i]).append("'");
+            if (i + 1 < a.length) sb.append(", ");
+        }
+        sb.append("]");
+        System.out.println(sb);
+    }
+}
