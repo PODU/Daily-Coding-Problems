4b3b138c145d505f22ea8848f844bc49005f4e73
Poduval Aditya Anil <podu2997@gmail.com>
2025-08-01 18:01:28 +0530

    Day #61 (Go)

---
+++ Day #61/solution.go
@@ -0,0 +1,23 @@
+// Fast (binary) exponentiation: square-and-multiply. Time O(log y), Space O(1).
+package main
+
+import "fmt"
+
+func fastPow(x, y int64) int64 {
+	if y < 0 {
+		y = -y
+	}
+	var result int64 = 1
+	for y > 0 {
+		if y&1 == 1 {
+			result *= x
+		}
+		x *= x
+		y >>= 1
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(fastPow(2, 10)) // 1024
+}
