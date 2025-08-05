0bfe06b80a67cf2ab53077d76f9930f9d181bc55
Poduval Aditya Anil <podu2997@gmail.com>
2025-08-05 22:55:37 +0530

    Day #74 (JavaScript)

---
+++ Day #74/solution.js
@@ -0,0 +1,11 @@
+// Count occurrences of X in N×N table: for each row i (1..N), X appears iff i|X and X/i in [1..N].
+// Time O(N), Space O(1).
+function countX(N, X) {
+  let cnt = 0;
+  for (let i = 1; i <= N; i++) {
+    if (X % i === 0 && X / i >= 1 && X / i <= N) cnt++;
+  }
+  return cnt;
+}
+
+console.log(countX(6, 12));
