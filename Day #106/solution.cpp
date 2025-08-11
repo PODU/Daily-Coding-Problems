a5a8ef6902c1bc039f736c3cce968ae95345f4f3
Poduval Aditya Anil <podu2997@gmail.com>
2025-08-11 20:37:29 +0530

    Day #106: C++ solution

---
+++ Day #106/solution.cpp
@@ -0,0 +1,16 @@
+// Day 106: Jump Game - greedy max-reach. O(n) time, O(1) space.
+#include <bits/stdc++.h>
+using namespace std;
+bool canReach(const vector<int>& a){
+    int reach = 0;
+    for (int i = 0; i < (int)a.size(); ++i){
+        if (i > reach) return false;
+        reach = max(reach, i + a[i]);
+    }
+    return true;
+}
+int main(){
+    cout << (canReach({2,0,1,0}) ? "True" : "False") << "\n";
+    cout << (canReach({1,1,0,1}) ? "True" : "False") << "\n";
+    return 0;
+}
