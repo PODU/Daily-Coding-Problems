4622fde7f3d7c9353a297f0a743541aaf9262917
Poduval Aditya Anil <podu2997@gmail.com>
2025-08-11 20:48:54 +0530

    Day #106: Rust solution

---
+++ Day #106/solution.rs
@@ -0,0 +1,16 @@
+// Day 106: Jump Game - greedy max-reach. O(n) time, O(1) space.
+fn can_reach(a: &[i32]) -> bool {
+    let mut reach: i32 = 0;
+    for (i, &x) in a.iter().enumerate() {
+        if i as i32 > reach {
+            return false;
+        }
+        reach = reach.max(i as i32 + x);
+    }
+    true
+}
+
+fn main() {
+    println!("{}", if can_reach(&[2, 0, 1, 0]) { "True" } else { "False" });
+    println!("{}", if can_reach(&[1, 1, 0, 1]) { "True" } else { "False" });
+}
