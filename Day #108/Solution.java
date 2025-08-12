f00820261deff791be21da88b645a9cb2569ffcb
Poduval Aditya Anil <podu2997@gmail.com>
2025-08-12 10:33:11 +0530

    Day #108: Java solution

---
+++ Day #108/Solution.java
@@ -0,0 +1,10 @@
+// Day 108: B is a rotation of A iff |A|==|B| and B is a substring of A+A. O(n).
+public class Solution {
+    static boolean isRotation(String a, String b){
+        return a.length() == b.length() && (a + a).contains(b);
+    }
+    public static void main(String[] args){
+        System.out.println(isRotation("abcde", "cdeab"));
+        System.out.println(isRotation("abc", "acb"));
+    }
+}
