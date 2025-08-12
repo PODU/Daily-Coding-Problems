de9126bdd1a09a3410b5fcc8fedd73b8a83e71d0
Poduval Aditya Anil <podu2997@gmail.com>
2025-08-12 10:55:42 +0530

    Day #108 (Go)

---
+++ Day #108/solution.go
@@ -0,0 +1,16 @@
+// Day 108: B is a rotation of A iff |A|==|B| and B is a substring of A+A. O(n).
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func isRotation(a, b string) bool {
+	return len(a) == len(b) && strings.Contains(a+a, b)
+}
+
+func main() {
+	fmt.Println(isRotation("abcde", "cdeab"))
+	fmt.Println(isRotation("abc", "acb"))
+}
