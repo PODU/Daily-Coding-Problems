411083dfe083d1dd857b42fc02e3e56f26911083
Poduval Aditya Anil <podu2997@gmail.com>
2025-08-19 20:48:22 +0530

    Day #145 (Go)

---
+++ Day #145/solution.go
@@ -0,0 +1,40 @@
+// Swap every two adjacent nodes in a singly linked list via iterative pointer rewiring. O(n) time, O(1) space.
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Node struct {
+	val  int
+	next *Node
+}
+
+func swapPairs(head *Node) *Node {
+	dummy := &Node{}
+	dummy.next = head
+	prev := dummy
+	for prev.next != nil && prev.next.next != nil {
+		a := prev.next
+		b := a.next
+		a.next = b.next
+		b.next = a
+		prev.next = b
+		prev = a
+	}
+	return dummy.next
+}
+
+func main() {
+	head := &Node{val: 1}
+	head.next = &Node{val: 2}
+	head.next.next = &Node{val: 3}
+	head.next.next.next = &Node{val: 4}
+	head = swapPairs(head)
+	var parts []string
+	for c := head; c != nil; c = c.next {
+		parts = append(parts, fmt.Sprintf("%d", c.val))
+	}
+	fmt.Println(strings.Join(parts, " -> ")) // 2 -> 1 -> 4 -> 3
+}
