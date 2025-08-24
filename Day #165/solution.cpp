ede08ddff1daea535b80dc7f0bd3337978827515
Poduval Aditya Anil <podu2997@gmail.com>
2025-08-24 12:18:29 +0530

    Day #165 (C++)

---
+++ Day #165/solution.cpp
@@ -0,0 +1,36 @@
+// Day 165: Count smaller elements to the right. Coordinate-compress, then sweep
+// right to left with a Fenwick tree (BIT). Time O(n log n), Space O(n).
+#include <bits/stdc++.h>
+using namespace std;
+
+struct BIT {
+    vector<int> t;
+    BIT(int n) : t(n + 1, 0) {}
+    void update(int i, int v) { for (; i < (int)t.size(); i += i & -i) t[i] += v; }
+    int query(int i) { int s = 0; for (; i > 0; i -= i & -i) s += t[i]; return s; }
+};
+
+vector<int> countSmaller(const vector<int>& a) {
+    int n = a.size();
+    vector<int> sorted(a);
+    sort(sorted.begin(), sorted.end());
+    sorted.erase(unique(sorted.begin(), sorted.end()), sorted.end());
+    BIT bit(sorted.size());
+    vector<int> res(n);
+    for (int i = n - 1; i >= 0; i--) {
+        int rank = lower_bound(sorted.begin(), sorted.end(), a[i]) - sorted.begin() + 1;
+        res[i] = bit.query(rank - 1);
+        bit.update(rank, 1);
+    }
+    return res;
+}
+
+int main() {
+    vector<int> a = {3, 4, 9, 6, 1};
+    vector<int> r = countSmaller(a);
+    cout << "[";
+    for (int i = 0; i < (int)r.size(); i++)
+        cout << r[i] << (i + 1 < (int)r.size() ? ", " : "");
+    cout << "]\n"; // [1, 1, 2, 1, 0]
+    return 0;
+}
