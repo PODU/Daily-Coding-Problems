7585916ddd9a1f160dcc5d5680a57ddcf3aa128c
Poduval Aditya Anil <podu2997@gmail.com>
2025-08-29 16:11:26 +0530

    Day #189 solved in Go

---
+++ Day #189/solution.go
@@ -0,0 +1,24 @@
+// Day 189: Longest contiguous subarray with all distinct elements.
+// Sliding window with last-seen map. Time O(n), Space O(n).
+package main
+
+import "fmt"
+
+func longestDistinct(a []int) int {
+	last := make(map[int]int)
+	best, start := 0, 0
+	for i, x := range a {
+		if p, ok := last[x]; ok && p >= start {
+			start = p + 1
+		}
+		last[x] = i
+		if i-start+1 > best {
+			best = i - start + 1
+		}
+	}
+	return best
+}
+
+func main() {
+	fmt.Println(longestDistinct([]int{5, 1, 3, 5, 2, 3, 4, 1}))
+}
