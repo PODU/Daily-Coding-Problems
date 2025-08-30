02bc815425b3350c0206052186cad3bd6f133021
Poduval Aditya Anil <podu2997@gmail.com>
2025-08-30 19:17:27 +0530

    Day #195 solved in Go

---
+++ Day #195/solution.go
@@ -0,0 +1,39 @@
+// Day 195: In a row- and column-sorted matrix, count elements < M[i1,j1] or > M[i2,j2].
+// Staircase counting of "<= x". Time O(N+M) per query, Space O(1).
+// Note: the README example counts the lower bound inclusively (expected 15), so we use
+// "<= M[i1,j1]" for the smaller side and strict "> M[i2,j2]" for the larger side.
+package main
+
+import "fmt"
+
+func countLessEqual(A [][]int, x int) int {
+	n, m := len(A), len(A[0])
+	r, c, cnt := 0, m-1, 0
+	for r < n && c >= 0 {
+		if A[r][c] <= x {
+			cnt += c + 1
+			r++
+		} else {
+			c--
+		}
+	}
+	return cnt
+}
+
+func solve(A [][]int, i1, j1, i2, j2 int) int {
+	n, m := len(A), len(A[0])
+	smaller := countLessEqual(A, A[i1][j1])
+	larger := n*m - countLessEqual(A, A[i2][j2])
+	return smaller + larger
+}
+
+func main() {
+	A := [][]int{
+		{1, 3, 7, 10, 15, 20},
+		{2, 6, 9, 14, 22, 25},
+		{3, 8, 10, 15, 25, 30},
+		{10, 11, 12, 23, 30, 35},
+		{20, 25, 30, 35, 40, 45},
+	}
+	fmt.Println(solve(A, 1, 1, 3, 3))
+}
