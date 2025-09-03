b982d1d0d85ef67d9370a4baeea0cf6494d383ec
Poduval Aditya Anil <podu2997@gmail.com>
2025-09-03 16:12:08 +0530

    Day #209: Go solution

---
+++ Day #209/solution.go
@@ -0,0 +1,42 @@
+// Day 209: Longest common subsequence of three strings.
+// 3D DP over prefixes: dp[i][j][k]. Time: O(n1*n2*n3), Space: O(n1*n2*n3).
+package main
+
+import "fmt"
+
+func max3(a, b, c int) int {
+	if b > a {
+		a = b
+	}
+	if c > a {
+		a = c
+	}
+	return a
+}
+
+func lcs3(a, b, c string) int {
+	n1, n2, n3 := len(a), len(b), len(c)
+	dp := make([][][]int, n1+1)
+	for i := range dp {
+		dp[i] = make([][]int, n2+1)
+		for j := range dp[i] {
+			dp[i][j] = make([]int, n3+1)
+		}
+	}
+	for i := 1; i <= n1; i++ {
+		for j := 1; j <= n2; j++ {
+			for k := 1; k <= n3; k++ {
+				if a[i-1] == b[j-1] && b[j-1] == c[k-1] {
+					dp[i][j][k] = dp[i-1][j-1][k-1] + 1
+				} else {
+					dp[i][j][k] = max3(dp[i-1][j][k], dp[i][j-1][k], dp[i][j][k-1])
+				}
+			}
+		}
+	}
+	return dp[n1][n2][n3]
+}
+
+func main() {
+	fmt.Println(lcs3("epidemiologist", "refrigeration", "supercalifragilisticexpialodocious")) // 5
+}
