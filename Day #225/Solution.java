6f0f3801f377439c740c6ac09104caddca8f3b44
Poduval Aditya Anil <podu2997@gmail.com>
2025-09-06 14:41:52 +0530

    Day #225: Java solution

---
+++ Day #225/Solution.java
@@ -0,0 +1,18 @@
+// Day 225: Josephus problem - position (1-indexed) of last survivor.
+// Approach: general O(N) recurrence J(i)=(J(i-1)+k)%i. Bonus: k==2 closed form O(log N): 2*(n-2^floor(log2 n))+1.
+public class Solution {
+    static int josephus(int n, int k) {
+        if (k == 2) {
+            int p = 1;
+            while (p * 2 <= n) p *= 2; // highest power of 2 <= n
+            return 2 * (n - p) + 1;
+        }
+        int res = 0; // 0-indexed
+        for (int i = 2; i <= n; i++) res = (res + k) % i;
+        return res + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(josephus(5, 2)); // 3
+    }
+}
