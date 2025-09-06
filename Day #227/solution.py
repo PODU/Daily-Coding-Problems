70002f6ad450480cb6c0448dd468b3345a0b19c8
Poduval Aditya Anil <podu2997@gmail.com>
2025-09-06 22:12:19 +0530

    Day #227: Python solution

---
+++ Day #227/solution.py
@@ -0,0 +1,45 @@
+# Day 227: Boggle solver: build a Trie of the dictionary, DFS from every cell over 8 neighbours.
+# Time: O(cells * 8^L) bounded by Trie; Space: O(dictionary size).
+
+
+def boggle(board, dict_words):
+    trie = {}
+    for w in dict_words:
+        node = trie
+        for ch in w:
+            node = node.setdefault(ch, {})
+        node['$'] = w
+    rows, cols = len(board), len(board[0])
+    found = set()
+
+    def dfs(r, c, node):
+        ch = board[r][c]
+        if ch not in node:
+            return
+        nxt = node[ch]
+        if '$' in nxt:
+            found.add(nxt['$'])
+        board[r][c] = '#'
+        for dr in (-1, 0, 1):
+            for dc in (-1, 0, 1):
+                if dr == 0 and dc == 0:
+                    continue
+                nr, nc = r + dr, c + dc
+                if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] != '#':
+                    dfs(nr, nc, nxt)
+        board[r][c] = ch
+
+    for r in range(rows):
+        for c in range(cols):
+            dfs(r, c, trie)
+    return sorted(found)
+
+
+if __name__ == "__main__":
+    board = [
+        ['o', 'a', 'a', 'n'],
+        ['e', 't', 'a', 'e'],
+        ['i', 'h', 'k', 'r'],
+        ['i', 'f', 'l', 'v']]
+    dict_words = ["oath", "pea", "eat", "rain"]
+    print(boggle(board, dict_words))
