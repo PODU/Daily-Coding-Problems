d82449b8299ed790d05b814a21b89938dea2d456
Poduval Aditya Anil <podu2997@gmail.com>
2025-09-10 11:19:14 +0530

    Solve Day #245 in Go

---
+++ Day #245/solution.go
@@ -0,0 +1,30 @@
+// Minimum jumps to reach end: greedy window/BFS-level expansion. O(n) time, O(1) space.
+package main
+
+import "fmt"
+
+func minJumps(a []int) int {
+	n := len(a)
+	if n <= 1 {
+		return 0
+	}
+	jumps, curEnd, farthest := 0, 0, 0
+	for i := 0; i < n-1; i++ {
+		if i+a[i] > farthest {
+			farthest = i + a[i]
+		}
+		if i == curEnd {
+			jumps++
+			curEnd = farthest
+			if curEnd >= n-1 {
+				break
+			}
+		}
+	}
+	return jumps
+}
+
+func main() {
+	a := []int{6, 2, 4, 0, 5, 1, 1, 4, 2, 9}
+	fmt.Println(minJumps(a))
+}
