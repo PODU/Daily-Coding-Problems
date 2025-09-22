eddbb17d2e06289ac54974f6ee1c2a6593018eb5
Poduval Aditya Anil <podu2997@gmail.com>
2025-09-22 18:46:52 +0530

    Day #315 solved in Rust

---
+++ Day #315/solution.rs
@@ -0,0 +1,21 @@
+// Day 315: Toeplitz check - each cell equals its top-left neighbor. O(N*M) time, O(1) space.
+fn is_toeplitz(m: &[Vec<i32>]) -> bool {
+    for i in 1..m.len() {
+        for j in 1..m[i].len() {
+            if m[i][j] != m[i - 1][j - 1] {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn main() {
+    let m = vec![
+        vec![1, 2, 3, 4, 8],
+        vec![5, 1, 2, 3, 4],
+        vec![4, 5, 1, 2, 3],
+        vec![7, 4, 5, 1, 2],
+    ];
+    println!("{}", if is_toeplitz(&m) { "True" } else { "False" }); // True
+}
