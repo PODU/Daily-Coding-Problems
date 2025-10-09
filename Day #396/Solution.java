5bd1372d5b2b60c3c658f7537029970fea5b8239
Poduval Aditya Anil <podu2997@gmail.com>
2025-10-09 11:55:37 +0530

    Day #396 solved in Java

---
+++ Day #396/Solution.java
@@ -0,0 +1,24 @@
+// Longest palindromic subsequence via interval DP dp[i][j] over s[i..j].
+// Time O(n^2), Space O(n^2).
+public class Solution {
+    static int lps(String s) {
+        int n = s.length();
+        if (n == 0) return 0;
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) dp[i][i] = 1;
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i + len - 1 < n; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j))
+                    dp[i][j] = 2 + (len == 2 ? 0 : dp[i + 1][j - 1]);
+                else
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[0][n - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lps("MAPTPTMTPA"));
+    }
+}
