890bec31da3a3e0331576321392e159bfe7b4411
Poduval Aditya Anil <podu2997@gmail.com>
2025-10-10 14:33:39 +0530

    Day #404 solved in Java

---
+++ Day #404/Solution.java
@@ -0,0 +1,25 @@
+// Min meeting rooms: sort starts and ends, two-pointer sweep, half-open [start,end).
+// Time O(n log n), Space O(n).
+import java.util.*;
+
+public class Solution {
+    static int minRooms(int[][] intervals) {
+        int n = intervals.length;
+        int[] starts = new int[n], ends = new int[n];
+        for (int i = 0; i < n; i++) { starts[i] = intervals[i][0]; ends[i] = intervals[i][1]; }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        int rooms = 0, maxRooms = 0, j = 0;
+        for (int i = 0; i < n; i++) {
+            while (j < n && ends[j] <= starts[i]) { rooms--; j++; }
+            rooms++;
+            maxRooms = Math.max(maxRooms, rooms);
+        }
+        return maxRooms;
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = {{30,75},{0,50},{60,150}};
+        System.out.println(minRooms(intervals));
+    }
+}
