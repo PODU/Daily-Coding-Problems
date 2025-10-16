9484a2618547f61577bafefd3afc0abf62438193
Poduval Aditya Anil <podu2997@gmail.com>
2025-10-16 11:57:28 +0530

    Day #441 solved in Rust

---
+++ Day #441/solution.rs
@@ -0,0 +1,25 @@
+// Day 441: Three-way partition around pivot x into <x, ==x, >x buckets.
+// O(n) time, O(n) space (stable bucket order matches the example).
+
+fn partition_three(lst: &[i32], x: i32) -> Vec<i32> {
+    let mut less = Vec::new();
+    let mut equal = Vec::new();
+    let mut greater = Vec::new();
+    for &v in lst {
+        if v < x {
+            less.push(v);
+        } else if v == x {
+            equal.push(v);
+        } else {
+            greater.push(v);
+        }
+    }
+    less.extend(equal);
+    less.extend(greater);
+    less
+}
+
+fn main() {
+    println!("{:?}", partition_three(&[9, 12, 3, 5, 14, 10, 10], 10));
+    // [9, 3, 5, 10, 10, 12, 14]
+}
