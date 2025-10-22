c1612a3fe6425b91fdd205c8eacca7428825e5bd
Poduval Aditya Anil <podu2997@gmail.com>
2025-10-22 11:22:04 +0530

    Day #472 solved in Java

---
+++ Day #472/Solution.java
@@ -0,0 +1,23 @@
+// Count decodings (a=1..z=26) with bottom-up DP keeping only two running states.
+// Time: O(n), Space: O(1).
+public class Solution {
+    static int numDecodings(String s) {
+        if (s.isEmpty() || s.charAt(0) == '0') return 0;
+        int prev2 = 1; // ways up to i-2
+        int prev1 = 1; // ways up to i-1
+        for (int i = 1; i < s.length(); i++) {
+            int cur = 0;
+            if (s.charAt(i) != '0') cur += prev1;
+            int two = (s.charAt(i - 1) - '0') * 10 + (s.charAt(i) - '0');
+            if (two >= 10 && two <= 26) cur += prev2;
+            prev2 = prev1;
+            prev1 = cur;
+        }
+        return prev1;
+    }
+
+    public static void main(String[] args) {
+        String msg = "111";
+        System.out.println(numDecodings(msg));
+    }
+}
