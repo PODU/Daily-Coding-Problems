72931149b24ef6d290713b28b842ab0305fe6ab1
Poduval Aditya Anil <podu2997@gmail.com>
2025-10-31 13:08:23 +0530

    Day #522: Python solution

---
+++ Day #522/solution.py
@@ -0,0 +1,34 @@
+# Day 522: KMP: build prefix-function for pattern, scan text. O(n+m) time, O(m) space.
+def kmp_search(s, p):
+    m = len(p)
+    lps = [0] * m
+    length = 0
+    i = 1
+    while i < m:
+        if p[i] == p[length]:
+            length += 1
+            lps[i] = length
+            i += 1
+        elif length:
+            length = lps[length - 1]
+        else:
+            lps[i] = 0
+            i += 1
+    res = []
+    i = j = 0
+    while i < len(s):
+        if s[i] == p[j]:
+            i += 1
+            j += 1
+            if j == m:
+                res.append(i - m)
+                j = lps[j - 1]
+        elif j:
+            j = lps[j - 1]
+        else:
+            i += 1
+    return res
+
+
+if __name__ == "__main__":
+    print(kmp_search("abracadabra", "abr"))
