58b3d4d768860d0918135c1316eb629a687ee394
Poduval Aditya Anil <podu2997@gmail.com>
2025-11-04 13:23:42 +0530

    Day #552: C++ solution

---
+++ Day #552/solution.cpp
@@ -0,0 +1,23 @@
+// Domino + Tromino tiling of 2xN: f(n)=2*f(n-1)+f(n-3), f(0)=1,f(1)=1,f(2)=2.
+// O(n) time, O(1) space.
+#include <bits/stdc++.h>
+using namespace std;
+
+long long tilings(int n) {
+    if (n == 0) return 1;
+    if (n == 1) return 1;
+    if (n == 2) return 2;
+    long long a = 1, b = 1, c = 2; // f(0),f(1),f(2)
+    long long cur = c;
+    for (int i = 3; i <= n; i++) {
+        cur = 2 * c + a;
+        a = b; b = c; c = cur;
+    }
+    return cur;
+}
+
+int main() {
+    int N = 4;
+    cout << tilings(N) << "\n"; // 11
+    return 0;
+}
