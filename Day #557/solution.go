fdfed0e8e52d1865fe482779f759d33ca33aceb2
Poduval Aditya Anil <podu2997@gmail.com>
2025-11-05 14:19:35 +0530

    Day #557 (Go)

---
+++ Day #557/solution.go
@@ -0,0 +1,22 @@
+// Count occurrences of X in an N x N multiplication table.
+// For each row i (1..N), X appears iff i divides X and X/i is in [1,N]. O(N) time, O(1) space.
+package main
+
+import "fmt"
+
+func countX(n int, x int) int {
+	cnt := 0
+	for i := 1; i <= n; i++ {
+		if x%i == 0 {
+			q := x / i
+			if q >= 1 && q <= n {
+				cnt++
+			}
+		}
+	}
+	return cnt
+}
+
+func main() {
+	fmt.Println(countX(6, 12)) // 4
+}
