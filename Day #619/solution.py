0d78c824e191a63c294c7b63a120b7a4f2ae7a5a
Poduval Aditya Anil <podu2997@gmail.com>
2025-11-18 12:57:18 +0530

    Day #619 (Python)

---
+++ Day #619/solution.py
@@ -0,0 +1,31 @@
+# Day 619: Word Search: DFS backtracking from each cell, mark visited then restore.
+# Time O(M*N*4^L), Space O(L) recursion depth.
+
+
+def exists(board, word):
+    rows, cols = len(board), len(board[0])
+
+    def dfs(i, j, k):
+        if k == len(word):
+            return True
+        if i < 0 or j < 0 or i >= rows or j >= cols or board[i][j] != word[k]:
+            return False
+        tmp = board[i][j]
+        board[i][j] = '#'
+        found = (dfs(i + 1, j, k + 1) or dfs(i - 1, j, k + 1) or
+                 dfs(i, j + 1, k + 1) or dfs(i, j - 1, k + 1))
+        board[i][j] = tmp
+        return found
+
+    for i in range(rows):
+        for j in range(cols):
+            if dfs(i, j, 0):
+                return True
+    return False
+
+
+if __name__ == "__main__":
+    board = [['A', 'B', 'C', 'E'], ['S', 'F', 'C', 'S'], ['A', 'D', 'E', 'E']]
+    print(str(exists([row[:] for row in board], "ABCCED")).lower())
+    print(str(exists([row[:] for row in board], "SEE")).lower())
+    print(str(exists([row[:] for row in board], "ABCB")).lower())
