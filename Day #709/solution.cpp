02be2f5da98d2e4a4c7fc0e624828bc67cd43c0e
Poduval Aditya Anil <podu2997@gmail.com>
2025-12-07 12:23:39 +0530

    Day #709 solved in C++

---
+++ Day #709/solution.cpp
@@ -0,0 +1,23 @@
+// Day 709: Count paths top-left to bottom-right moving right/down, avoiding walls.
+// DP: dp[i][j] = dp[i-1][j] + dp[i][j-1], 0 at walls. Time O(N*M), space O(M).
+#include <bits/stdc++.h>
+using namespace std;
+
+long long countPaths(const vector<vector<int>>& g) {
+    int n = g.size(), m = g[0].size();
+    vector<long long> dp(m, 0);
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (g[i][j] == 1) { dp[j] = 0; continue; }
+            if (i == 0 && j == 0) dp[j] = 1;
+            else dp[j] = (j > 0 ? dp[j - 1] : 0) + dp[j];
+        }
+    }
+    return dp[m - 1];
+}
+
+int main() {
+    vector<vector<int>> grid = {{0, 0, 1}, {0, 0, 1}, {1, 0, 0}};
+    cout << countPaths(grid) << endl;
+    return 0;
+}
