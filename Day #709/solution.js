e75ff906f9d2837cda55ba053994d2b6aa45897f
Poduval Aditya Anil <podu2997@gmail.com>
2025-12-07 14:20:55 +0530

    Add JavaScript solution for Day #709

---
+++ Day #709/solution.js
@@ -0,0 +1,17 @@
+// Day 709: Count paths top-left to bottom-right moving right/down, avoiding walls.
+// DP: dp[i][j] = dp[i-1][j] + dp[i][j-1], 0 at walls. Time O(N*M), space O(M).
+function countPaths(g) {
+  const n = g.length, m = g[0].length;
+  const dp = new Array(m).fill(0);
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (g[i][j] === 1) dp[j] = 0;
+      else if (i === 0 && j === 0) dp[j] = 1;
+      else dp[j] = (j > 0 ? dp[j - 1] : 0) + dp[j];
+    }
+  }
+  return dp[m - 1];
+}
+
+const grid = [[0, 0, 1], [0, 0, 1], [1, 0, 0]];
+console.log(countPaths(grid));
