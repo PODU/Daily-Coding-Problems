90832ef70b2047d1121d27cb6eaf5919ff654452
Poduval Aditya Anil <podu2997@gmail.com>
2025-12-08 15:20:37 +0530

    Day #716: JavaScript solution

---
+++ Day #716/solution.js
@@ -0,0 +1,23 @@
+// Day 716: Validate UTF-8. Read leading byte to get char length (1-4) from its
+// high bits, then verify each continuation byte starts with 10. Time O(n).
+function validUtf8(data) {
+  let i = 0;
+  const n = data.length;
+  while (i < n) {
+    const b = data[i] & 0xFF;
+    let len;
+    if (b >> 7 === 0b0) len = 1;
+    else if (b >> 5 === 0b110) len = 2;
+    else if (b >> 4 === 0b1110) len = 3;
+    else if (b >> 3 === 0b11110) len = 4;
+    else return false;
+    if (i + len > n) return false;
+    for (let j = 1; j < len; j++)
+      if ((data[i + j] & 0xFF) >> 6 !== 0b10) return false;
+    i += len;
+  }
+  return true;
+}
+
+console.log(validUtf8([226, 130, 172]) ? "True" : "False");
+console.log(validUtf8([235, 140, 4]) ? "True" : "False");
