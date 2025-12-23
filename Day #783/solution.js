6e96f62c0a17680769a559542a1f7904fca840e7
Poduval Aditya Anil <podu2997@gmail.com>
2025-12-23 10:13:58 +0530

    Day #783 solved in JavaScript

---
+++ Day #783/solution.js
@@ -0,0 +1,20 @@
+// Longest Increasing Subsequence (length), patience sorting.
+// Maintain tails[]; binary-search insertion point for each value. O(n log n) time, O(n) space.
+
+function lengthOfLIS(nums) {
+  const tails = [];
+  for (const x of nums) {
+    let lo = 0, hi = tails.length;
+    while (lo < hi) {
+      const mid = (lo + hi) >> 1;
+      if (tails[mid] < x) lo = mid + 1;
+      else hi = mid;
+    }
+    if (lo === tails.length) tails.push(x);
+    else tails[lo] = x;
+  }
+  return tails.length;
+}
+
+const nums = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15];
+console.log(lengthOfLIS(nums));
