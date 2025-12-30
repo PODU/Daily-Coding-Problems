9b41a4357af6c0c9d2d8a332d2b565aca4c7b858
Poduval Aditya Anil <podu2997@gmail.com>
2025-12-30 11:17:24 +0530

    Day #820 solved in Java

---
+++ Day #820/Solution.java
@@ -0,0 +1,21 @@
+// First missing positive via cyclic sort: place nums[i] at index nums[i]-1, then scan. O(n) time, O(1) space.
+public class Solution {
+    static int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                int t = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = t;
+            }
+        }
+        for (int i = 0; i < n; i++)
+            if (nums[i] != i + 1) return i + 1;
+        return n + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstMissingPositive(new int[]{3, 4, -1, 1}));
+        System.out.println(firstMissingPositive(new int[]{1, 2, 0}));
+    }
+}
