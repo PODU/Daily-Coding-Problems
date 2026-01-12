4a9ff4824343f15239e6b12351a32942929acbeb
Poduval Aditya Anil <podu2997@gmail.com>
2026-01-12 15:03:20 +0530

    Solve Day #885 in Java

---
+++ Day #885/Solution.java
@@ -0,0 +1,25 @@
+// Dutch national flag: three-pointer in-place partition R<G<B. Time O(n), Space O(1).
+import java.util.*;
+
+public class Solution {
+    static void sortRGB(char[] a){
+        int low = 0, mid = 0, high = a.length - 1;
+        while(mid <= high){
+            if(a[mid] == 'R'){ char t = a[low]; a[low++] = a[mid]; a[mid++] = t; }
+            else if(a[mid] == 'G'){ mid++; }
+            else { char t = a[mid]; a[mid] = a[high]; a[high--] = t; } // 'B'
+        }
+    }
+
+    public static void main(String[] args){
+        char[] a = {'G', 'B', 'R', 'R', 'B', 'R', 'G'};
+        sortRGB(a);
+        StringBuilder sb = new StringBuilder("[");
+        for(int i = 0; i < a.length; i++){
+            sb.append("'").append(a[i]).append("'");
+            if(i + 1 < a.length) sb.append(", ");
+        }
+        sb.append("]");
+        System.out.println(sb);
+    }
+}
