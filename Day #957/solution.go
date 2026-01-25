eaab4495cc461dd175f4cf3cad33afa3ca1a2fc7
Poduval Aditya Anil <podu2997@gmail.com>
2026-01-25 16:43:34 +0530

    Day #957 solved in Go

---
+++ Day #957/solution.go
@@ -0,0 +1,69 @@
+// Day 957: skyline problem via sweep line with a max-heap (lazy deletion).
+// Time O(n log n), Space O(n).
+package main
+
+import (
+	"container/heap"
+	"fmt"
+	"sort"
+	"strings"
+)
+
+type maxHeap []int
+
+func (h maxHeap) Len() int            { return len(h) }
+func (h maxHeap) Less(i, j int) bool  { return h[i] > h[j] }
+func (h maxHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
+func (h *maxHeap) Push(x interface{}) { *h = append(*h, x.(int)) }
+func (h *maxHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[:n-1]
+	return x
+}
+
+func skyline(buildings [][3]int) [][2]int {
+	type ev struct{ x, h int }
+	var events []ev
+	for _, b := range buildings {
+		events = append(events, ev{b[0], -b[2]}, ev{b[1], b[2]})
+	}
+	sort.Slice(events, func(i, j int) bool {
+		if events[i].x != events[j].x {
+			return events[i].x < events[j].x
+		}
+		return events[i].h < events[j].h
+	})
+	h := &maxHeap{0}
+	removed := map[int]int{}
+	prev := 0
+	var res [][2]int
+	for _, e := range events {
+		if e.h < 0 {
+			heap.Push(h, -e.h)
+		} else {
+			removed[e.h]++
+		}
+		for h.Len() > 0 && removed[(*h)[0]] > 0 {
+			removed[(*h)[0]]--
+			heap.Pop(h)
+		}
+		cur := (*h)[0]
+		if cur != prev {
+			res = append(res, [2]int{e.x, cur})
+			prev = cur
+		}
+	}
+	return res
+}
+
+func main() {
+	res := skyline([][3]int{{0, 15, 3}, {4, 11, 5}, {19, 23, 4}})
+	var parts []string
+	for _, p := range res {
+		parts = append(parts, fmt.Sprintf("(%d, %d)", p[0], p[1]))
+	}
+	fmt.Println("[" + strings.Join(parts, ", ") + "]")
+	// [(0, 3), (4, 5), (11, 3), (15, 0), (19, 4), (23, 0)]
+}
