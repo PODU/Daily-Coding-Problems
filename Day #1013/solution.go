5bd153f34beb62529c2019334293ef2602b22db7
Poduval Aditya Anil <podu2997@gmail.com>
2026-02-03 16:13:37 +0530

    Solve Day #1013 in Go

---
+++ Day #1013/solution.go
@@ -0,0 +1,20 @@
+// Bitwise AND of all ints in [M,N] = common binary prefix of M and N.
+// Shift both right until equal, then shift back. Time: O(log N), Space: O(1).
+package main
+
+import "fmt"
+
+func rangeAnd(m, n int) int {
+	shift := 0
+	for m < n {
+		m >>= 1
+		n >>= 1
+		shift++
+	}
+	return m << shift
+}
+
+func main() {
+	fmt.Printf("AND(5, 7) = %d\n", rangeAnd(5, 7))      // 4
+	fmt.Printf("AND(12, 15) = %d\n", rangeAnd(12, 15))  // 12
+}
