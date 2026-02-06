1980996227bf1160ab28ee29e75f68be48079d70
Poduval Aditya Anil <podu2997@gmail.com>
2026-02-06 13:54:09 +0530

    Day #1029 solved in Java

---
+++ Day #1029/Solution.java
@@ -0,0 +1,17 @@
+// Day 1029: Minimum coins for n cents with {1,5,10,25}. Greedy is optimal for
+// this canonical US denomination set. Time O(#denominations), Space O(1).
+public class Solution {
+    static int minCoins(int n) {
+        int[] coins = {25, 10, 5, 1};
+        int count = 0;
+        for (int c : coins) {
+            count += n / c;
+            n %= c;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(minCoins(16));
+    }
+}
