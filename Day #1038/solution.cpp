1e79bed91dc66edce989c6f486e19f59b88d4fbd
Poduval Aditya Anil <podu2997@gmail.com>
2026-02-07 18:33:16 +0530

    Solve Day #1038 in C++

---
+++ Day #1038/solution.cpp
@@ -0,0 +1,26 @@
+// Reverse word order in-place: reverse whole char array, then reverse each word.
+// Time: O(n), Space: O(1) extra.
+#include <bits/stdc++.h>
+using namespace std;
+
+void reverseRange(string& s, int i, int j) {
+    while (i < j) swap(s[i++], s[j--]);
+}
+
+void reverseWords(string& s) {
+    reverseRange(s, 0, (int)s.size() - 1);
+    int start = 0, n = (int)s.size();
+    for (int i = 0; i <= n; i++) {
+        if (i == n || s[i] == ' ') {
+            reverseRange(s, start, i - 1);
+            start = i + 1;
+        }
+    }
+}
+
+int main() {
+    string s = "hello world here";
+    reverseWords(s);
+    cout << "\"" << s << "\"" << "\n";
+    return 0;
+}
