abb77a563ece660770e240dea940062538e47938
Poduval Aditya Anil <podu2997@gmail.com>
2026-02-17 11:35:17 +0530

    Day #1087 solved in C++

---
+++ Day #1087/solution.cpp
@@ -0,0 +1,12 @@
+// B is a rotation of A iff lengths match and B is a substring of A+A. Time O(n), Space O(n).
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isRotation(const string& a, const string& b) {
+    return a.size() == b.size() && (a + a).find(b) != string::npos;
+}
+
+int main() {
+    cout << (isRotation("abcde", "cdeab") ? "true" : "false") << "\n";
+    cout << (isRotation("abc", "acb") ? "true" : "false") << "\n";
+}
