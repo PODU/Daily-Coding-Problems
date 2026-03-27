3ace6a44ec894909c65ae382c614c591079a73eb
Poduval Aditya Anil <podu2997@gmail.com>
2026-03-27 15:55:10 +0530

    Solve Day #1272 in Java

---
+++ Day #1272/Solution.java
@@ -0,0 +1,15 @@
+// Day 1272: Determine whether a matrix is Toeplitz (each TL->BR diagonal is constant).
+// Compare every cell with its upper-left neighbor. O(N*M) time, O(1) space.
+public class Solution {
+    static boolean isToeplitz(int[][] m) {
+        for (int i = 1; i < m.length; i++)
+            for (int j = 1; j < m[i].length; j++)
+                if (m[i][j] != m[i - 1][j - 1]) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[][] m = {{1,2,3,4,8},{5,1,2,3,4},{4,5,1,2,3},{7,4,5,1,2}};
+        System.out.println(isToeplitz(m));
+    }
+}
