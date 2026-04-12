32188a3006f7777b77b150950e8b7f46a7ffa8ad
Poduval Aditya Anil <podu2997@gmail.com>
2026-04-12 14:45:38 +0530

    Day #1346 solved in C++

---
+++ Day #1346/solution.cpp
@@ -0,0 +1,20 @@
+// Expand outward from index i, returning nearest j (by |j-i|) with a[j] > a[i]; -1 if none.
+// Time: O(n) per query, Space: O(1).
+#include <bits/stdc++.h>
+using namespace std;
+
+int nearestLarger(const vector<int>& a, int i) {
+    int n = a.size();
+    for (int d = 1; d < n; ++d) {
+        int l = i - d, r = i + d;
+        if (l >= 0 && a[l] > a[i]) return l;
+        if (r < n && a[r] > a[i]) return r;
+    }
+    return -1;
+}
+
+int main() {
+    vector<int> a = {4, 1, 3, 5, 6};
+    cout << nearestLarger(a, 0) << "\n";
+    return 0;
+}
