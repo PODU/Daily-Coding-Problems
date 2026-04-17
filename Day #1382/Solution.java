3dc8bfb9d0393b3319dc5238d23bfb3de79a17eb
Poduval Aditya Anil <podu2997@gmail.com>
2026-04-17 22:21:53 +0530

    Day #1382 (Java)

---
+++ Day #1382/Solution.java
@@ -0,0 +1,25 @@
+// LIS length via patience sorting: maintain tails[], binary search (lower_bound) for strict increase. O(n log n) time, O(n) space.
+import java.util.*;
+
+public class Solution {
+    static int lengthOfLIS(int[] nums) {
+        int[] tails = new int[nums.length];
+        int size = 0;
+        for (int x : nums) {
+            int lo = 0, hi = size;
+            while (lo < hi) {
+                int mid = (lo + hi) >>> 1;
+                if (tails[mid] < x) lo = mid + 1;
+                else hi = mid;
+            }
+            tails[lo] = x;
+            if (lo == size) size++;
+        }
+        return size;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        System.out.println(lengthOfLIS(nums));
+    }
+}
