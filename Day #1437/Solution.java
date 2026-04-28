49b41f2c7fb3d33a948315dfbeba0d378db0cf56
Poduval Aditya Anil <podu2997@gmail.com>
2026-04-28 19:03:39 +0530

    Day #1437 (Java)

---
+++ Day #1437/Solution.java
@@ -0,0 +1,27 @@
+// Day 1437: Length of longest strictly increasing subsequence.
+// Approach: Patience sorting; maintain tails array, binary search lower_bound.
+// Time: O(n log n), Space: O(n).
+import java.util.*;
+
+public class Solution {
+    static int lengthOfLIS(int[] nums) {
+        int[] tails = new int[nums.length];
+        int size = 0;
+        for (int x : nums) {
+            int lo = 0, hi = size;
+            while (lo < hi) {
+                int mid = (lo + hi) >>> 1;
+                if (tails[mid] < x) lo = mid + 1;
+                else hi = mid;
+            }
+            tails[lo] = x;
+            if (lo == size) size++;
+        }
+        return size;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
+        System.out.println(lengthOfLIS(nums)); // 6
+    }
+}
