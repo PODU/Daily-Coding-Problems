6a03c7d684a18d64018f5f4c3f364079420bd0e8
Poduval Aditya Anil <podu2997@gmail.com>
2026-04-28 21:02:48 +0530

    Day #1437: Go solution

---
+++ Day #1437/solution.go
@@ -0,0 +1,27 @@
+// Day 1437: Length of longest strictly increasing subsequence.
+// Approach: Patience sorting; maintain tails array, binary search lower_bound.
+// Time: O(n log n), Space: O(n).
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func lengthOfLIS(nums []int) int {
+	tails := []int{}
+	for _, x := range nums {
+		i := sort.SearchInts(tails, x)
+		if i == len(tails) {
+			tails = append(tails, x)
+		} else {
+			tails[i] = x
+		}
+	}
+	return len(tails)
+}
+
+func main() {
+	nums := []int{0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15}
+	fmt.Println(lengthOfLIS(nums)) // 6
+}
