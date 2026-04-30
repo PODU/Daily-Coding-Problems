4712f46b147773604dabd4cf02444d00c098fda4
Poduval Aditya Anil <podu2997@gmail.com>
2026-04-30 21:25:41 +0530

    Day #1449 (Go)

---
+++ Day #1449/solution.go
@@ -0,0 +1,31 @@
+// Day 1449: Trapping Rain Water. Two-pointer sweep tracking running left/right
+// maxima. Time O(n), Space O(1).
+package main
+
+import "fmt"
+
+func trap(h []int) int {
+	l, r := 0, len(h)-1
+	leftMax, rightMax, water := 0, 0, 0
+	for l < r {
+		if h[l] < h[r] {
+			if h[l] > leftMax {
+				leftMax = h[l]
+			}
+			water += leftMax - h[l]
+			l++
+		} else {
+			if h[r] > rightMax {
+				rightMax = h[r]
+			}
+			water += rightMax - h[r]
+			r--
+		}
+	}
+	return water
+}
+
+func main() {
+	fmt.Println(trap([]int{2, 1, 2}))          // 1
+	fmt.Println(trap([]int{3, 0, 1, 3, 0, 5})) // 8
+}
