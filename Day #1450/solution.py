2be812558ac6d4135d8d1318755d54ea2623d167
Poduval Aditya Anil <podu2997@gmail.com>
2026-05-01 11:23:29 +0530

    Day #1450 solved in Python

---
+++ Day #1450/solution.py
@@ -0,0 +1,12 @@
+# Day 1450: Tower of Hanoi - print each single-disk move from rod 1 to rod 3.
+# Classic recursion. Time O(2^n) moves, Space O(n) recursion depth.
+def hanoi(n, frm, to, aux):
+    if n == 0:
+        return
+    hanoi(n - 1, frm, aux, to)
+    print(f"Move {frm} to {to}")
+    hanoi(n - 1, aux, to, frm)
+
+
+if __name__ == "__main__":
+    hanoi(3, 1, 3, 2)
