8682862a5a3d1b61e4a025b5198d4c046a53c89c
Poduval Aditya Anil <podu2997@gmail.com>
2026-05-01 11:25:55 +0530

    Day #1450 (Java)

---
+++ Day #1450/Solution.java
@@ -0,0 +1,14 @@
+// Day 1450: Tower of Hanoi - print each single-disk move from rod 1 to rod 3.
+// Classic recursion. Time O(2^n) moves, Space O(n) recursion depth.
+public class Solution {
+    static void hanoi(int n, int from, int to, int aux) {
+        if (n == 0) return;
+        hanoi(n - 1, from, aux, to);
+        System.out.println("Move " + from + " to " + to);
+        hanoi(n - 1, aux, to, from);
+    }
+
+    public static void main(String[] args) {
+        hanoi(3, 1, 3, 2);
+    }
+}
