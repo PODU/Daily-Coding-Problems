12b9a55aa4190064331f9036e57835ccf3f1cfbc
Poduval Aditya Anil <podu2997@gmail.com>
2026-05-08 21:24:30 +0530

    Add Java solution for Day #1492

---
+++ Day #1492/Solution.java
@@ -0,0 +1,25 @@
+// Minimum meeting rooms for overlapping intervals.
+// Sort starts and ends, two-pointer sweep. Time O(n log n), Space O(n).
+import java.util.Arrays;
+
+public class Solution {
+    static int minRooms(int[][] intervals) {
+        int n = intervals.length;
+        int[] starts = new int[n], ends = new int[n];
+        for (int i = 0; i < n; i++) { starts[i] = intervals[i][0]; ends[i] = intervals[i][1]; }
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+        int rooms = 0, maxRooms = 0, j = 0;
+        for (int i = 0; i < n; i++) {
+            while (j < n && ends[j] <= starts[i]) { rooms--; j++; }
+            rooms++;
+            maxRooms = Math.max(maxRooms, rooms);
+        }
+        return maxRooms;
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = {{30,75},{0,50},{60,150}};
+        System.out.println(minRooms(intervals)); // expected: 2
+    }
+}
