ba3ec3aa34e202cf814ebe08c7f2d4b1bca2c918
Poduval Aditya Anil <podu2997@gmail.com>
2026-05-19 17:23:24 +0530

    Add Java solution for Day #1535

---
+++ Day #1535/Solution.java
@@ -0,0 +1,19 @@
+// Two numbers summing to k via a single-pass hash set.
+// Time O(n), Space O(n).
+import java.util.*;
+
+public class Solution {
+    static boolean hasPairSum(int[] nums, int k) {
+        Set<Integer> seen = new HashSet<>();
+        for (int x : nums) {
+            if (seen.contains(k - x)) return true;
+            seen.add(x);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 15, 3, 7};
+        System.out.println(hasPairSum(nums, 17));
+    }
+}
