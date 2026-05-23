a31ad30527b2f44a4a42e7f4ea87f25bd1661b8f
Poduval Aditya Anil <podu2997@gmail.com>
2026-05-23 17:51:03 +0530

    Day #1549 (Java)

---
+++ Day #1549/Solution.java
@@ -0,0 +1,20 @@
+// Toeplitz check: every element equals its top-left neighbor matrix[i-1][j-1].
+// Time O(m*n), Space O(1).
+public class Solution {
+    static boolean isToeplitz(int[][] m) {
+        for (int i = 1; i < m.length; i++)
+            for (int j = 1; j < m[i].length; j++)
+                if (m[i][j] != m[i - 1][j - 1]) return false;
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[][] m = {
+            {1, 2, 3, 4, 8},
+            {5, 1, 2, 3, 4},
+            {4, 5, 1, 2, 3},
+            {7, 4, 5, 1, 2}
+        };
+        System.out.println(isToeplitz(m));
+    }
+}
