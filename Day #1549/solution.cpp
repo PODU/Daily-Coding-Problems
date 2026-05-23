8108aa50c85eb4ea7911c0eca0a3b8c04ed0882f
Poduval Aditya Anil <podu2997@gmail.com>
2026-05-23 17:43:42 +0530

    Solve Day #1549 in C++

---
+++ Day #1549/solution.cpp
@@ -0,0 +1,23 @@
+// Toeplitz check: every element equals its top-left neighbor matrix[i-1][j-1].
+// Time O(m*n), Space O(1).
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isToeplitz(const vector<vector<int>>& m) {
+    for (size_t i = 1; i < m.size(); i++)
+        for (size_t j = 1; j < m[i].size(); j++)
+            if (m[i][j] != m[i - 1][j - 1]) return false;
+    return true;
+}
+
+int main() {
+    vector<vector<int>> m = {
+        {1, 2, 3, 4, 8},
+        {5, 1, 2, 3, 4},
+        {4, 5, 1, 2, 3},
+        {7, 4, 5, 1, 2}
+    };
+    cout << (isToeplitz(m) ? "true" : "false") << "\n";
+    return 0;
+}
