529441f743f5aa200384c03146cd3440d28b92f1
Poduval Aditya Anil <podu2997@gmail.com>
2026-05-23 18:35:53 +0530

    Day #1549 (Rust)

---
+++ Day #1549/solution.rs
@@ -0,0 +1,23 @@
+// Toeplitz check: every element equals its top-left neighbor matrix[i-1][j-1].
+// Time O(m*n), Space O(1).
+
+fn is_toeplitz(m: &[Vec<i32>]) -> bool {
+    for i in 1..m.len() {
+        for j in 1..m[i].len() {
+            if m[i][j] != m[i - 1][j - 1] {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn main() {
+    let m = vec![
+        vec![1, 2, 3, 4, 8],
+        vec![5, 1, 2, 3, 4],
+        vec![4, 5, 1, 2, 3],
+        vec![7, 4, 5, 1, 2],
+    ];
+    println!("{}", if is_toeplitz(&m) { "true" } else { "false" });
+}
