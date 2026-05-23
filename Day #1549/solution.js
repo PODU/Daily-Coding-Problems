a9cdbb1d93e759287fd116b2ae4914cc5ad25a0f
Poduval Aditya Anil <podu2997@gmail.com>
2026-05-23 18:44:00 +0530

    Solve Day #1549 in JavaScript

---
+++ Day #1549/solution.js
@@ -0,0 +1,17 @@
+// Toeplitz check: every element equals its top-left neighbor matrix[i-1][j-1].
+// Time O(m*n), Space O(1).
+
+function isToeplitz(m) {
+  for (let i = 1; i < m.length; i++)
+    for (let j = 1; j < m[i].length; j++)
+      if (m[i][j] !== m[i - 1][j - 1]) return false;
+  return true;
+}
+
+const m = [
+  [1, 2, 3, 4, 8],
+  [5, 1, 2, 3, 4],
+  [4, 5, 1, 2, 3],
+  [7, 4, 5, 1, 2],
+];
+console.log(isToeplitz(m) ? "true" : "false");
