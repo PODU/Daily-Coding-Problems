2ab1bc45218690c6ce0c1244002edbf98bb652af
Poduval Aditya Anil <podu2997@gmail.com>
2026-05-31 16:46:48 +0530

    Add Go solution for Day #1587

---
+++ Day #1587/solution.go
@@ -0,0 +1,31 @@
+// Minimum jumps to reach the end. Greedy O(n): track current reachable end & farthest.
+// Time: O(n); Space: O(1).
+package main
+
+import "fmt"
+
+func minJumps(a []int) int {
+	n := len(a)
+	if n <= 1 {
+		return 0
+	}
+	jumps, curEnd, farthest := 0, 0, 0
+	for i := 0; i < n-1; i++ {
+		if i+a[i] > farthest {
+			farthest = i + a[i]
+		}
+		if i == curEnd {
+			jumps++
+			curEnd = farthest
+			if curEnd >= n-1 {
+				break
+			}
+		}
+	}
+	return jumps
+}
+
+func main() {
+	a := []int{6, 2, 4, 0, 5, 1, 1, 4, 2, 9}
+	fmt.Println(minJumps(a))
+}
