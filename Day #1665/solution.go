9c3046404d097bfeddfeee9b372aa67a6a2a701d
Poduval Aditya Anil <podu2997@gmail.com>
2026-06-15 12:28:01 +0530

    Day #1665 solved in Go

---
+++ Day #1665/solution.go
@@ -0,0 +1,21 @@
+// Brick wall: prefix-sum edge positions per row, max edge frequency => fewest cuts = rows - maxEdges. O(total bricks) time/space.
+package main
+
+import "fmt"
+
+func main() {
+	wall := [][]int{{3, 5, 1, 1}, {2, 3, 3, 2}, {5, 5}, {4, 4, 2}, {1, 3, 3, 3}, {1, 1, 6, 1, 1}}
+	freq := make(map[int]int)
+	best := 0
+	for _, row := range wall {
+		sum := 0
+		for i := 0; i+1 < len(row); i++ {
+			sum += row[i]
+			freq[sum]++
+			if freq[sum] > best {
+				best = freq[sum]
+			}
+		}
+	}
+	fmt.Println(len(wall) - best)
+}
