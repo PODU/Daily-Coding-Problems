a2c77dd75cb79976588aed8e6732c824805f62c5
Poduval Aditya Anil <podu2997@gmail.com>
2026-07-02 10:49:11 +0530

    Day #1748: Go solution

---
+++ Day #1748/solution.go
@@ -0,0 +1,39 @@
+// Day 1748: Recover digits from an anagram of concatenated number words (zero-nine).
+// Approach: unique-letter signatures (z,w,u,x,g first; then derive odd digits). O(n) time, O(1) space.
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func recover_(s string) string {
+	var c [26]int
+	for _, ch := range s {
+		c[ch-'a']++
+	}
+	g := func(ch byte) int { return c[ch-'a'] }
+	cnt := make([]int, 10)
+	cnt[0] = g('z')                          // zero
+	cnt[2] = g('w')                          // two
+	cnt[4] = g('u')                          // four
+	cnt[6] = g('x')                          // six
+	cnt[8] = g('g')                          // eight
+	cnt[3] = g('h') - cnt[8]                 // three
+	cnt[5] = g('f') - cnt[4]                 // five
+	cnt[7] = g('s') - cnt[6]                 // seven
+	cnt[1] = g('o') - cnt[0] - cnt[2] - cnt[4] // one
+	cnt[9] = g('i') - cnt[5] - cnt[6] - cnt[8] // nine
+
+	var sb strings.Builder
+	for d := 0; d <= 9; d++ {
+		for k := 0; k < cnt[d]; k++ {
+			sb.WriteByte(byte('0' + d))
+		}
+	}
+	return sb.String()
+}
+
+func main() {
+	fmt.Println(recover_("niesevehrtfeev")) // 357
+}
