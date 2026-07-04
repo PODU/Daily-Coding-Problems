aad8e5b60ae4e97e476125a6a6535e6ea305b34c
Poduval Aditya Anil <podu2997@gmail.com>
2026-07-04 20:01:54 +0530

    Day #1765: C++ solution

---
+++ Day #1765/solution.cpp
@@ -0,0 +1,20 @@
+// Roman numeral -> decimal: scan left to right; subtract when a symbol's value is
+// less than the next symbol's, otherwise add. Time: O(n), Space: O(1).
+#include <bits/stdc++.h>
+using namespace std;
+
+int romanToInt(const string& s) {
+    unordered_map<char, int> v = {{'M',1000},{'D',500},{'C',100},
+                                  {'L',50},{'X',10},{'V',5},{'I',1}};
+    int total = 0;
+    for (size_t i = 0; i < s.size(); i++) {
+        if (i + 1 < s.size() && v[s[i]] < v[s[i + 1]]) total -= v[s[i]];
+        else total += v[s[i]];
+    }
+    return total;
+}
+
+int main() {
+    cout << romanToInt("XIV") << "\n";
+    return 0;
+}
