63a0aa38bd00787894f0f6728e389054f9eebcb8
Poduval Aditya Anil <podu2997@gmail.com>
2026-07-04 20:04:34 +0530

    Add Java solution for Day #1765

---
+++ Day #1765/Solution.java
@@ -0,0 +1,24 @@
+// Roman numeral -> decimal: scan left to right; subtract when a symbol's value is
+// less than the next symbol's, otherwise add. Time: O(n), Space: O(1).
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    static int romanToInt(String s) {
+        Map<Character, Integer> v = new HashMap<>();
+        v.put('M', 1000); v.put('D', 500); v.put('C', 100);
+        v.put('L', 50); v.put('X', 10); v.put('V', 5); v.put('I', 1);
+        int total = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i + 1 < s.length() && v.get(s.charAt(i)) < v.get(s.charAt(i + 1)))
+                total -= v.get(s.charAt(i));
+            else
+                total += v.get(s.charAt(i));
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(romanToInt("XIV"));
+    }
+}
