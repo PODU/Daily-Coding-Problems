0009553207220bd8c393f9df7d10d2e9a32c0032
Poduval Aditya Anil <podu2997@gmail.com>
2026-07-04 20:38:31 +0530

    Day #1765: JavaScript solution

---
+++ Day #1765/solution.js
@@ -0,0 +1,14 @@
+// Roman numeral -> decimal: scan left to right; subtract when a symbol's value is
+// less than the next symbol's, otherwise add. Time: O(n), Space: O(1).
+
+function romanToInt(s) {
+  const v = { M: 1000, D: 500, C: 100, L: 50, X: 10, V: 5, I: 1 };
+  let total = 0;
+  for (let i = 0; i < s.length; i++) {
+    if (i + 1 < s.length && v[s[i]] < v[s[i + 1]]) total -= v[s[i]];
+    else total += v[s[i]];
+  }
+  return total;
+}
+
+console.log(romanToInt("XIV"));
