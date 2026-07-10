a56700af3af414146117badf982e21ee6dfcfeda
Poduval Aditya Anil <podu2997@gmail.com>
2026-07-10 11:39:06 +0530

    Add JavaScript solution for Day #1789

---
+++ Day #1789/solution.js
@@ -0,0 +1,25 @@
+// Min broadcast range: sort towers, binary-search nearest tower per listener, take max.
+// Time O((N+M) log M), Space O(1) extra.
+function lowerBound(arr, x) {
+  let lo = 0, hi = arr.length;
+  while (lo < hi) {
+    const mid = (lo + hi) >> 1;
+    if (arr[mid] < x) lo = mid + 1; else hi = mid;
+  }
+  return lo;
+}
+
+function minRange(listeners, towers) {
+  towers = towers.slice().sort((a, b) => a - b);
+  let ans = 0;
+  for (const L of listeners) {
+    const i = lowerBound(towers, L);
+    let best = Infinity;
+    if (i < towers.length) best = Math.min(best, towers[i] - L);
+    if (i > 0) best = Math.min(best, L - towers[i - 1]);
+    ans = Math.max(ans, best);
+  }
+  return ans;
+}
+
+console.log(minRange([1, 5, 11, 20], [4, 8, 15]));
