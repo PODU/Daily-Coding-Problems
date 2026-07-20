4428c1ae91a8f8020fabf465702e11ce98ebc041
Poduval Aditya Anil <podu2997@gmail.com>
2026-07-20 18:30:39 +0530

    Solve Day #1849 in Java

---
+++ Day #1849/Solution.java
@@ -0,0 +1,37 @@
+// Day 1849: SparseArray storing only non-zero entries in a hash map.
+// init O(N) once, set/get O(1) average. Space O(non-zero count).
+import java.util.*;
+
+public class Solution {
+    static class SparseArray {
+        private final Map<Integer, Integer> data = new HashMap<>();
+        private int size;
+
+        void init(int[] arr, int n) {
+            size = n;
+            data.clear();
+            for (int i = 0; i < arr.length; i++)
+                if (arr[i] != 0) data.put(i, arr[i]);
+        }
+        void set(int i, int val) {
+            if (i < 0 || i >= size) throw new IndexOutOfBoundsException();
+            if (val == 0) data.remove(i);
+            else data.put(i, val);
+        }
+        int get(int i) {
+            if (i < 0 || i >= size) throw new IndexOutOfBoundsException();
+            return data.getOrDefault(i, 0);
+        }
+    }
+
+    public static void main(String[] args) {
+        SparseArray sa = new SparseArray();
+        sa.init(new int[]{0, 0, 5, 0, 0, 0, 9, 0}, 8);
+        System.out.println(sa.get(2)); // 5
+        System.out.println(sa.get(3)); // 0
+        sa.set(3, 7);
+        System.out.println(sa.get(3)); // 7
+        sa.set(2, 0);
+        System.out.println(sa.get(2)); // 0
+    }
+}
