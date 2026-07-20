c423e744b2e8c723b661d5199abeef33364663de
Poduval Aditya Anil <podu2997@gmail.com>
2026-07-20 18:46:12 +0530

    Add C++ solution for Day #1849

---
+++ Day #1849/solution.cpp
@@ -0,0 +1,37 @@
+// Day 1849: SparseArray storing only non-zero entries in a hash map.
+// init O(N) once, set/get O(1) average. Space O(non-zero count).
+#include <bits/stdc++.h>
+using namespace std;
+
+class SparseArray {
+    unordered_map<int, int> data;
+    int size;
+public:
+    void init(const vector<int>& arr, int n) {
+        size = n;
+        data.clear();
+        for (int i = 0; i < (int)arr.size(); i++)
+            if (arr[i] != 0) data[i] = arr[i];
+    }
+    void set(int i, int val) {
+        if (i < 0 || i >= size) throw out_of_range("index");
+        if (val == 0) data.erase(i);
+        else data[i] = val;
+    }
+    int get(int i) {
+        if (i < 0 || i >= size) throw out_of_range("index");
+        auto it = data.find(i);
+        return it == data.end() ? 0 : it->second;
+    }
+};
+
+int main() {
+    SparseArray sa;
+    sa.init({0, 0, 5, 0, 0, 0, 9, 0}, 8);
+    cout << sa.get(2) << "\n";  // 5
+    cout << sa.get(3) << "\n";  // 0
+    sa.set(3, 7);
+    cout << sa.get(3) << "\n";  // 7
+    sa.set(2, 0);
+    cout << sa.get(2) << "\n";  // 0
+}
