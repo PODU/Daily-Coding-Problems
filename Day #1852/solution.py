16ac79249f1041684ebe4c42c873276270e37f80
Poduval Aditya Anil <podu2997@gmail.com>
2026-07-21 12:06:20 +0530

    Solve Day #1852 in Python

---
+++ Day #1852/solution.py
@@ -0,0 +1,18 @@
+# Day 1852: Longest Increasing Subsequence (strict).
+# Patience sorting: maintain tails[]; bisect_left for insertion point. O(n log n) time, O(n) space.
+from bisect import bisect_left
+
+
+def lis(a):
+    tails = []
+    for x in a:
+        i = bisect_left(tails, x)
+        if i == len(tails):
+            tails.append(x)
+        else:
+            tails[i] = x
+    return len(tails)
+
+
+if __name__ == "__main__":
+    print(lis([10, 9, 2, 5, 3, 7, 101, 18]))  # 4
